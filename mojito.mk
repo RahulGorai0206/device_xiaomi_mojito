68e403f02db511fc5ddcba58fe71e67806d720f0
TejasKamat <kamattejas23@gmail.com>
2022-04-13 14:59:42 +0530

    mojito: zephyrus-ify
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- mojito.mk
+++ mojito.mk
@@ -11,14 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common zephyrus stuffs
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_mojito
+PRODUCT_NAME := mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
